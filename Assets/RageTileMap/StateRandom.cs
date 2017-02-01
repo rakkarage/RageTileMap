4bfb59e5788b5c376c3036d4ea3bfd45db5cf3c3
rakkarage <rakkarage@gmail.com>
2017-02-01 18:13:08 -0500

    x

---
+++ Assets/RageTileMap/StateRandom.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+public class StateRandom : StateMachineBehaviour
+{
+	static int AnimatorRandom = Animator.StringToHash("Random");
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		animator.SetFloat(AnimatorRandom, Random.Range(0f, 1f));
+	}
+}
